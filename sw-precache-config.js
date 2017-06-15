aee54813678f45241b6ce8f2b430e0fbd2ded9d6
agarwalamit662 <agarwalamit662@gmail.com>
2017-06-15 15:09:16 +0530

    First Commit

---
+++ sw-precache-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/*',
+    '/src/img/*'
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          name: 'webcomponentsjs-polyfills-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/images\/.*/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
+};
